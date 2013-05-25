eb77dfb01756fc6737d93e21077772f989593615
Gustav <sir.gustav.the.coder@gmail.com>
2013-05-26 01:12:55 +0200

    cpplint fixes for math.h

--- euphoria/math.h
+++ euphoria/math.h
@@ -6,8 +6,12 @@
 
 #include "cml/cml.h"
 
-typedef cml::vector<float, cml::fixed<3> > vec3;
-// typedef cml::matrix<float, cml::fixed<4,4>, cml::col_basis, cml::col_major> mat44;
-typedef cml::matrix44f_c mat44;
+// cpplint complain that we don't include <vector> but we use cml::vector
+//   here so <vector> isn't needed
+typedef cml::vector < float,  // NOLINT(build/include_what_you_use)
+        cml::fixed<3> > vec3;
 
-#endif // EUPHORIA_MATH_H_
+typedef cml::matrix < float, cml::fixed<4, 4>,
+        cml::col_basis, cml::col_major > mat44;
+
+#endif  // EUPHORIA_MATH_H_
