96843c155878241bb01390f89d368939e0656c68
Gustav <sir.gustav.the.coder@gmail.com>
2013-06-02 00:15:45 +0200

    removed bitmap generation

--- euphoria/bitmap.h
+++ euphoria/bitmap.h
@@ -36,13 +36,6 @@ class Bitmap
       Rgba
     };
 
-    /** Construct a new Bitmap.
-    @param width the width of the image.
-    @param height the height of the image.
-    @param type the RGB type of the image.
-     */
-    Bitmap(Size width, Size height, Type type);
-
     /** Load a bitmap from file.
     @param path the path to the bitmap.
      */
@@ -51,31 +44,6 @@ class Bitmap
     /** Destruct the bitmap.
      */
     ~Bitmap();
-
-    /** Paints a pixel in the image.
-    This takes the pixel and changes it depending on the color and alpha of the input and modifying it.
-    @param x the x coordinate of the pixel.
-    @param y the y coordinate of the pixel.
-    @param c the color to fuse with the current pixel.
-    @see setPixel
-     */
-    void paintPixel(Size x, Size y, const Color& c);
-
-    /** Set the color of the pixel in a image.
-    This ignores the previous color of the pixel and sets it to the new color.
-    @param x the x coordinate of the pixel.
-    @param y the y coordinate of the pixel.
-    @param c the color of the new pixel.
-    @see paintPixel
-     */
-    void setPixel(Size x, Size y, const Color& c);
-
-    /** Get the color of a single pixel.
-    @param x the x coordinate of the pixel.
-    @param y the y coordinate of the pixel.
-    @return the color of the pixel.
-     */
-    const Color getPixel(Size x, Size y) const;
 
     /** Get the width of the image.
     @return the width.
