ca3ca3b94741f91ed11e64f329e5685e73446760
Gustav <sir.gustav.the.coder@gmail.com>
2013-06-21 01:24:50 +0200

    added string enum type

---
+++ euphoria/enum.cpp
@@ -0,0 +1,110 @@
+// Euphoria - Copyright (c) Gustav
+
+#include "euphoria/enum.h"
+
+#include <cassert>
+#include <string>
+
+EnumType::EnumType()
+  : isAdding(true)
+  , nextIndex(0) {
+}
+
+EnumType::~EnumType() {
+  assert(isAdding == false);
+  assert(createdButNotAddedList.empty() == true);
+}
+
+const std::string& EnumType::toString(size_t v) const {
+  assert(v < nextIndex);
+  List::const_iterator f = list.find(v);
+  if (f != list.end()) {
+    return f->second;
+  } else {
+    List::const_iterator i = createdButNotAddedList.find(v);
+    if (i == createdButNotAddedList.end()) {
+      throw "unknown index";
+    } else {
+      return i->second;
+    }
+  }
+}
+
+const EnumValue EnumType::toEnum(const std::string& name) {
+  Map::const_iterator r = map.find(name);
+  if (r == map.end()) {
+    if (isAdding) {
+      const size_t id = nextIndex;
+      ++nextIndex;
+      createdButNotAddedList.insert(List::value_type(id, name));
+      createdButNotAddedMap.insert(Map::value_type(name, id));
+      map.insert(Map::value_type(name, id));
+      return EnumValue(this, id);
+    } else {
+      throw "loading has finished, enum doesnt exist or is misspelled";
+    }
+  } else {
+    return EnumValue(this, r->second);
+  }
+}
+
+void EnumType::addEnum(const std::string& name) {
+  assert(isAdding == true);
+  Map::const_iterator r = map.find(name);
+  if (r == map.end()) {
+    const size_t id = nextIndex;
+    ++nextIndex;
+    list.insert(List::value_type(id, name));
+    map.insert(Map::value_type(name, id));
+  } else {
+    Map::const_iterator f = createdButNotAddedMap.find(name);
+    if (f == createdButNotAddedMap.end()) {
+      throw "enum already added";
+    } else {
+      // move to list
+      const size_t id = f->second;
+      List::const_iterator i = createdButNotAddedList.find(id);
+      // createdButNotAdded list/map inconsistencies
+      assert(i != createdButNotAddedList.end());
+      createdButNotAddedList.erase(i);
+      createdButNotAddedMap.erase(f);
+    }
+  }
+}
+
+void EnumType::stopAdding() {
+  assert(isAdding == true);
+  isAdding = false;
+  assert(createdButNotAddedList.empty() == true);  // if this isn't empty,
+  // some enums have not been added or misspelling has occurred,
+  // see throw above
+}
+
+EnumValue::EnumValue(EnumType* t, size_t v)
+  : type(t)
+  , value(v) {
+}
+
+const std::string EnumValue::toString() const {
+  return type->toString(value);
+}
+
+const size_t EnumValue::toValue() const {
+  return value;
+}
+
+bool EnumValue::operator==(const EnumValue& other) const {
+  assert(type == other.type);
+  return value == other.value;
+}
+bool EnumValue::operator!=(const EnumValue& other) const {
+  return !(*this == other);
+}
+bool EnumValue::operator<(const EnumValue& other) const {
+  assert(type == other.type);
+  return value < other.value;
+}
+std::ostream& operator<<(std::ostream& s, const EnumValue& v) {
+  s << v.toString();
+  return s;
+}
---
+++ euphoria/enum.h
@@ -0,0 +1,114 @@
+// Euphoria - Copyright (c) Gustav
+
+#ifndef EUPHORIA_ENUM_H_
+#define EUPHORIA_ENUM_H_
+
+#include <string>
+#include <map>
+#include <vector>
+
+
+class EnumValue;
+
+/** Represents a enum type.
+Declare globally grab specific enums before or after load, and load adds new,
+verifies created and asserts misspelled values.
+@todo move internal into a private/protected place with friends.
+ */
+class EnumType {
+  public:
+    /** Constructor.
+     */
+    EnumType();
+
+    /** Destructor.
+     */
+    ~EnumType();
+
+    /** Internal.
+    Construct a string from the enum type representation.
+    @param v the enum.
+    @returns the string representation.
+     */
+    const std::string& toString(size_t v) const;
+
+    /** Creates or gets a enum value.
+    @param name the unique name of the value.
+    @returns the enum value.
+     */
+    const EnumValue toEnum(const std::string& name);
+
+    /** Adds a valid enum value.
+    Pretty much only usable during loading.
+    @param name the unique name.
+     */
+    void addEnum(const std::string& name);
+
+    /** Stops adding values to the enum.
+    Puts the type in static mode, meaning no new values can be added and added
+    values are verified against the valid values.
+     */
+    void stopAdding();
+
+  private:
+    typedef std::map<size_t, std::string> List;
+    typedef std::map<std::string, size_t> Map;
+    List list;
+    Map map;
+
+    List createdButNotAddedList;
+    Map createdButNotAddedMap;
+    bool isAdding;
+    size_t nextIndex;
+};
+
+/** A enum value.
+@todo move internal into a private/protected place with friends.
+ */
+class EnumValue {
+  public:
+    /** Internal constructor.
+    @param type the type of the value.
+    @param value the actual value.
+     */
+    EnumValue(EnumType* type, size_t value);
+
+    /** Create a string representation.
+    Useful for debugging and displaying stuff.
+    @returns the string representation.
+     */
+    const std::string toString() const;
+
+    /** Gets the actual value.
+    @return the value.
+     */
+    const size_t toValue() const;
+
+    /** Equal operator.
+    @param other the other value.
+    @returns true if equal, false if not.
+     */
+    bool operator==(const EnumValue& other) const;
+
+    /** Not equal operator.
+    @param other the other value.
+    @returns true if not equal, false if not.
+     */
+    bool operator!=(const EnumValue& other) const;
+
+    /** Less than operator.
+    @param other the other value.
+    @returns true if this is less than the other, false if not.
+     */
+    bool operator<(const EnumValue& other) const;
+
+  private:
+    /// @todo investigate consts
+    EnumType* type;
+    size_t value;
+};
+
+std::ostream& operator<<(std::ostream& s, const EnumValue& v);
+
+
+#endif  // EUPHORIA_ENUM_H_
