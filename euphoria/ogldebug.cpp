ed309394e11a5f027e85fae906e484e40b62d627
Gustav <sir.gustav.the.coder@gmail.com>
2013-07-11 06:27:00 +0200

    fleshed out ogl exceptions

--- euphoria/ogldebug.cpp
+++ euphoria/ogldebug.cpp
@@ -1,9 +1,12 @@
 // Euphoria - Copyright (c) Gustav
 
-#include <string>
-
 #include "euphoria/ogldebug.h"
+
+#include <string>
+#include <stdexcept>
+
 #include "euphoria/opengl.h"
+#include "euphoria/str.h"
 
 // definitions shamelessly stolen from glew headers
 /* <winnt.h> */
@@ -19,22 +22,48 @@
 #endif
 
 /**
-@throws a character string when a error has occured.
+@throws a std::logic_error when a error has occurred.
  */
 void OglDebug::Verify() {
   const GLenum err = glGetError();
   if (err != GL_NO_ERROR) {
-    std::string name = "unknown gl error";
+    std::string name = "Unknown OpenGL error";
+    std::string description;
 
     switch (err) {
-#define ERR(x) case x: name = #x
-        ERR(GL_INVALID_OPERATION);
-        ERR(GL_INVALID_VALUE);
+#define ERR(x, d) case x: name = #x; description = d; break
+        ERR(GL_INVALID_ENUM,
+            "An unacceptable value is specified for an enumerated argument."
+            " The offending command is ignored"
+            " and has no other side effect than to set the error flag.");
+        ERR(GL_INVALID_VALUE,
+            "A numeric argument is out of range."
+            " The offending command is ignored"
+            " and has no other side effect than to set the error flag.");
+        ERR(GL_INVALID_OPERATION,
+            "The specified operation is not allowed in the current state."
+            " The offending command is ignored"
+            " and has no other side effect than to set the error flag.");
+        ERR(GL_INVALID_FRAMEBUFFER_OPERATION,
+            "The framebuffer object is not complete. The offending command"
+            " is ignored and has no other side effect than to set"
+            " the error flag.");
+        ERR(GL_OUT_OF_MEMORY,
+            "There is not enough memory left to execute the command."
+            " The state of the GL is undefined,"
+            " except for the state of the error flags,"
+            " after this error is recorded.");
+        ERR(GL_STACK_UNDERFLOW,
+            "An attempt has been made to perform an operation that would"
+            " cause an internal stack to underflow.");
+        ERR(GL_STACK_OVERFLOW,
+            "An attempt has been made to perform an operation that would"
+            " cause an internal stack to overflow.");
 #undef ERR
     };
 
-    /// @todo investigate what to throw here, should probably throw name instead
-    throw "gl error occurred";
+    throw std::logic_error(Str() << "Open GL error occurred "
+                           << name << ": " << description);
   }
 }
 
