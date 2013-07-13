62415b20f4b68c898be97c3ab237c9fd1abda256
Gustav <sir.gustav.the.coder@gmail.com>
2013-07-13 18:28:05 +0200

    added defined colors

---
+++ euphoria/colors.cpp
@@ -0,0 +1,429 @@
+// Euphoria - Copyright (c) Gustav
+
+#include "euphoria/colors.h"
+
+// Source: http://www.w3schools.com/html/html_colorvalues.asp
+
+namespace colors {
+
+  Color Black() {
+    return Color(0x000000);
+  }
+  Color Navy() {
+    return Color(0x000080);
+  }
+  Color DarkBlue() {
+    return Color(0x00008B);
+  }
+  Color MediumBlue() {
+    return Color(0x0000CD);
+  }
+  Color Blue() {
+    return Color(0x0000FF);
+  }
+  Color DarkGreen() {
+    return Color(0x006400);
+  }
+  Color Green() {
+    return Color(0x008000);
+  }
+  Color Teal() {
+    return Color(0x008080);
+  }
+  Color DarkCyan() {
+    return Color(0x008B8B);
+  }
+  Color DeepSkyBlue() {
+    return Color(0x00BFFF);
+  }
+  Color DarkTurquoise() {
+    return Color(0x00CED1);
+  }
+  Color MediumSpringGreen() {
+    return Color(0x00FA9A);
+  }
+  Color Lime() {
+    return Color(0x00FF00);
+  }
+  Color SpringGreen() {
+    return Color(0x00FF7F);
+  }
+  Color Aqua() {
+    return Color(0x00FFFF);
+  }
+  Color Cyan() {
+    return Color(0x00FFFF);
+  }
+  Color MidnightBlue() {
+    return Color(0x191970);
+  }
+  Color DodgerBlue() {
+    return Color(0x1E90FF);
+  }
+  Color LightSeaGreen() {
+    return Color(0x20B2AA);
+  }
+  Color ForestGreen() {
+    return Color(0x228B22);
+  }
+  Color SeaGreen() {
+    return Color(0x2E8B57);
+  }
+  Color DarkSlateGray() {
+    return Color(0x2F4F4F);
+  }
+  Color LimeGreen() {
+    return Color(0x32CD32);
+  }
+  Color MediumSeaGreen() {
+    return Color(0x3CB371);
+  }
+  Color Turquoise() {
+    return Color(0x40E0D0);
+  }
+  Color RoyalBlue() {
+    return Color(0x4169E1);
+  }
+  Color SteelBlue() {
+    return Color(0x4682B4);
+  }
+  Color DarkSlateBlue() {
+    return Color(0x483D8B);
+  }
+  Color MediumTurquoise() {
+    return Color(0x48D1CC);
+  }
+  Color Indigo() {
+    return Color(0x4B0082);
+  }
+  Color DarkOliveGreen() {
+    return Color(0x556B2F);
+  }
+  Color CadetBlue() {
+    return Color(0x5F9EA0);
+  }
+  Color CornflowerBlue() {
+    return Color(0x6495ED);
+  }
+  Color MediumAquaMarine() {
+    return Color(0x66CDAA);
+  }
+  Color DimGray() {
+    return Color(0x696969);
+  }
+  Color SlateBlue() {
+    return Color(0x6A5ACD);
+  }
+  Color OliveDrab() {
+    return Color(0x6B8E23);
+  }
+  Color SlateGray() {
+    return Color(0x708090);
+  }
+  Color LightSlateGray() {
+    return Color(0x778899);
+  }
+  Color MediumSlateBlue() {
+    return Color(0x7B68EE);
+  }
+  Color LawnGreen() {
+    return Color(0x7CFC00);
+  }
+  Color Chartreuse() {
+    return Color(0x7FFF00);
+  }
+  Color Aquamarine() {
+    return Color(0x7FFFD4);
+  }
+  Color Maroon() {
+    return Color(0x800000);
+  }
+  Color Purple() {
+    return Color(0x800080);
+  }
+  Color Olive() {
+    return Color(0x808000);
+  }
+  Color Gray() {
+    return Color(0x808080);
+  }
+  Color SkyBlue() {
+    return Color(0x87CEEB);
+  }
+  Color LightSkyBlue() {
+    return Color(0x87CEFA);
+  }
+  Color BlueViolet() {
+    return Color(0x8A2BE2);
+  }
+  Color DarkRed() {
+    return Color(0x8B0000);
+  }
+  Color DarkMagenta() {
+    return Color(0x8B008B);
+  }
+  Color SaddleBrown() {
+    return Color(0x8B4513);
+  }
+  Color DarkSeaGreen() {
+    return Color(0x8FBC8F);
+  }
+  Color LightGreen() {
+    return Color(0x90EE90);
+  }
+  Color MediumPurple() {
+    return Color(0x9370DB);
+  }
+  Color DarkViolet() {
+    return Color(0x9400D3);
+  }
+  Color PaleGreen() {
+    return Color(0x98FB98);
+  }
+  Color DarkOrchid() {
+    return Color(0x9932CC);
+  }
+  Color YellowGreen() {
+    return Color(0x9ACD32);
+  }
+  Color Sienna() {
+    return Color(0xA0522D);
+  }
+  Color Brown() {
+    return Color(0xA52A2A);
+  }
+  Color DarkGray() {
+    return Color(0xA9A9A9);
+  }
+  Color LightBlue() {
+    return Color(0xADD8E6);
+  }
+  Color GreenYellow() {
+    return Color(0xADFF2F);
+  }
+  Color PaleTurquoise() {
+    return Color(0xAFEEEE);
+  }
+  Color LightSteelBlue() {
+    return Color(0xB0C4DE);
+  }
+  Color PowderBlue() {
+    return Color(0xB0E0E6);
+  }
+  Color FireBrick() {
+    return Color(0xB22222);
+  }
+  Color DarkGoldenRod() {
+    return Color(0xB8860B);
+  }
+  Color MediumOrchid() {
+    return Color(0xBA55D3);
+  }
+  Color RosyBrown() {
+    return Color(0xBC8F8F);
+  }
+  Color DarkKhaki() {
+    return Color(0xBDB76B);
+  }
+  Color Silver() {
+    return Color(0xC0C0C0);
+  }
+  Color MediumVioletRed() {
+    return Color(0xC71585);
+  }
+  Color IndianRed() {
+    return Color(0xCD5C5C);
+  }
+  Color Peru() {
+    return Color(0xCD853F);
+  }
+  Color Chocolate() {
+    return Color(0xD2691E);
+  }
+  Color Tan() {
+    return Color(0xD2B48C);
+  }
+  Color LightGray() {
+    return Color(0xD3D3D3);
+  }
+  Color Thistle() {
+    return Color(0xD8BFD8);
+  }
+  Color Orchid() {
+    return Color(0xDA70D6);
+  }
+  Color GoldenRod() {
+    return Color(0xDAA520);
+  }
+  Color PaleVioletRed() {
+    return Color(0xDB7093);
+  }
+  Color Crimson() {
+    return Color(0xDC143C);
+  }
+  Color Gainsboro() {
+    return Color(0xDCDCDC);
+  }
+  Color Plum() {
+    return Color(0xDDA0DD);
+  }
+  Color BurlyWood() {
+    return Color(0xDEB887);
+  }
+  Color LightCyan() {
+    return Color(0xE0FFFF);
+  }
+  Color Lavender() {
+    return Color(0xE6E6FA);
+  }
+  Color DarkSalmon() {
+    return Color(0xE9967A);
+  }
+  Color Violet() {
+    return Color(0xEE82EE);
+  }
+  Color PaleGoldenRod() {
+    return Color(0xEEE8AA);
+  }
+  Color LightCoral() {
+    return Color(0xF08080);
+  }
+  Color Khaki() {
+    return Color(0xF0E68C);
+  }
+  Color AliceBlue() {
+    return Color(0xF0F8FF);
+  }
+  Color HoneyDew() {
+    return Color(0xF0FFF0);
+  }
+  Color Azure() {
+    return Color(0xF0FFFF);
+  }
+  Color SandyBrown() {
+    return Color(0xF4A460);
+  }
+  Color Wheat() {
+    return Color(0xF5DEB3);
+  }
+  Color Beige() {
+    return Color(0xF5F5DC);
+  }
+  Color WhiteSmoke() {
+    return Color(0xF5F5F5);
+  }
+  Color MintCream() {
+    return Color(0xF5FFFA);
+  }
+  Color GhostWhite() {
+    return Color(0xF8F8FF);
+  }
+  Color Salmon() {
+    return Color(0xFA8072);
+  }
+  Color AntiqueWhite() {
+    return Color(0xFAEBD7);
+  }
+  Color Linen() {
+    return Color(0xFAF0E6);
+  }
+  Color LightGoldenRodYellow() {
+    return Color(0xFAFAD2);
+  }
+  Color OldLace() {
+    return Color(0xFDF5E6);
+  }
+  Color Red() {
+    return Color(0xFF0000);
+  }
+  Color Fuchsia() {
+    return Color(0xFF00FF);
+  }
+  Color Magenta() {
+    return Color(0xFF00FF);
+  }
+  Color DeepPink() {
+    return Color(0xFF1493);
+  }
+  Color OrangeRed() {
+    return Color(0xFF4500);
+  }
+  Color Tomato() {
+    return Color(0xFF6347);
+  }
+  Color HotPink() {
+    return Color(0xFF69B4);
+  }
+  Color Coral() {
+    return Color(0xFF7F50);
+  }
+  Color DarkOrange() {
+    return Color(0xFF8C00);
+  }
+  Color LightSalmon() {
+    return Color(0xFFA07A);
+  }
+  Color Orange() {
+    return Color(0xFFA500);
+  }
+  Color LightPink() {
+    return Color(0xFFB6C1);
+  }
+  Color Pink() {
+    return Color(0xFFC0CB);
+  }
+  Color Gold() {
+    return Color(0xFFD700);
+  }
+  Color PeachPuff() {
+    return Color(0xFFDAB9);
+  }
+  Color NavajoWhite() {
+    return Color(0xFFDEAD);
+  }
+  Color Moccasin() {
+    return Color(0xFFE4B5);
+  }
+  Color Bisque() {
+    return Color(0xFFE4C4);
+  }
+  Color MistyRose() {
+    return Color(0xFFE4E1);
+  }
+  Color BlanchedAlmond() {
+    return Color(0xFFEBCD);
+  }
+  Color PapayaWhip() {
+    return Color(0xFFEFD5);
+  }
+  Color LavenderBlush() {
+    return Color(0xFFF0F5);
+  }
+  Color SeaShell() {
+    return Color(0xFFF5EE);
+  }
+  Color Cornsilk() {
+    return Color(0xFFF8DC);
+  }
+  Color LemonChiffon() {
+    return Color(0xFFFACD);
+  }
+  Color FloralWhite() {
+    return Color(0xFFFAF0);
+  }
+  Color Snow() {
+    return Color(0xFFFAFA);
+  }
+  Color Yellow() {
+    return Color(0xFFFF00);
+  }
+  Color LightYellow() {
+    return Color(0xFFFFE0);
+  }
+  Color Ivory() {
+    return Color(0xFFFFF0);
+  }
+  Color White() {
+    return Color(0xFFFFFF);
+  }
+}  // namespace colors
---
+++ euphoria/colors.h
@@ -0,0 +1,721 @@
+// Euphoria - Copyright (c) Gustav
+
+/** @file
+Utility code for genrating colors.
+ */
+
+#ifndef EUPHORIA_COLORS_H_
+#define EUPHORIA_COLORS_H_
+
+#include "euphoria/color.h"
+
+
+namespace colors {
+  /**
+  @defgroup colors Color values
+  @{
+   */
+  /** Returns the color Black.
+  @returns the color Black.
+  */
+  Color Black();
+
+  /** Returns the color Navy.
+  @returns the color Navy.
+  */
+  Color Navy();
+
+  /** Returns the color Dark Blue.
+  @returns the color Dark Blue.
+  */
+  Color DarkBlue();
+
+  /** Returns the color Medium Blue.
+  @returns the color Medium Blue.
+  */
+  Color MediumBlue();
+
+  /** Returns the color Blue.
+  @returns the color Blue.
+  */
+  Color Blue();
+
+  /** Returns the color Dark Green.
+  @returns the color Dark Green.
+  */
+  Color DarkGreen();
+
+  /** Returns the color Green.
+  @returns the color Green.
+  */
+  Color Green();
+
+  /** Returns the color Teal.
+  @returns the color Teal.
+  */
+  Color Teal();
+
+  /** Returns the color Dark Cyan.
+  @returns the color Dark Cyan.
+  */
+  Color DarkCyan();
+
+  /** Returns the color Deep Sky Blue.
+  @returns the color Deep Sky Blue.
+  */
+  Color DeepSkyBlue();
+
+  /** Returns the color Dark Turquoise.
+  @returns the color Dark Turquoise.
+  */
+  Color DarkTurquoise();
+
+  /** Returns the color Medium Spring Green.
+  @returns the color Medium Spring Green.
+  */
+  Color MediumSpringGreen();
+
+  /** Returns the color Lime.
+  @returns the color Lime.
+  */
+  Color Lime();
+
+  /** Returns the color Spring Green.
+  @returns the color Spring Green.
+  */
+  Color SpringGreen();
+
+  /** Returns the color Aqua.
+  @returns the color Aqua.
+  */
+  Color Aqua();
+
+  /** Returns the color Cyan.
+  @returns the color Cyan.
+  */
+  Color Cyan();
+
+  /** Returns the color Midnight Blue.
+  @returns the color Midnight Blue.
+  */
+  Color MidnightBlue();
+
+  /** Returns the color Dodger Blue.
+  @returns the color Dodger Blue.
+  */
+  Color DodgerBlue();
+
+  /** Returns the color Light Sea Green.
+  @returns the color Light Sea Green.
+  */
+  Color LightSeaGreen();
+
+  /** Returns the color Forest Green.
+  @returns the color Forest Green.
+  */
+  Color ForestGreen();
+
+  /** Returns the color Sea Green.
+  @returns the color Sea Green.
+  */
+  Color SeaGreen();
+
+  /** Returns the color Dark Slate Gray.
+  @returns the color Dark Slate Gray.
+  */
+  Color DarkSlateGray();
+
+  /** Returns the color Lime Green.
+  @returns the color Lime Green.
+  */
+  Color LimeGreen();
+
+  /** Returns the color Medium Sea Green.
+  @returns the color Medium Sea Green.
+  */
+  Color MediumSeaGreen();
+
+  /** Returns the color Turquoise.
+  @returns the color Turquoise.
+  */
+  Color Turquoise();
+
+  /** Returns the color Royal Blue.
+  @returns the color Royal Blue.
+  */
+  Color RoyalBlue();
+
+  /** Returns the color Steel Blue.
+  @returns the color Steel Blue.
+  */
+  Color SteelBlue();
+
+  /** Returns the color Dark Slate Blue.
+  @returns the color Dark Slate Blue.
+  */
+  Color DarkSlateBlue();
+
+  /** Returns the color Medium Turquoise.
+  @returns the color Medium Turquoise.
+  */
+  Color MediumTurquoise();
+
+  /** Returns the color Indigo.
+  @returns the color Indigo.
+  */
+  Color Indigo();
+
+  /** Returns the color Dark Olive Green.
+  @returns the color Dark Olive Green.
+  */
+  Color DarkOliveGreen();
+
+  /** Returns the color Cadet Blue.
+  @returns the color Cadet Blue.
+  */
+  Color CadetBlue();
+
+  /** Returns the color Cornflower Blue.
+  @returns the color Cornflower Blue.
+  */
+  Color CornflowerBlue();
+
+  /** Returns the color Medium Aqua Marine.
+  @returns the color Medium Aqua Marine.
+  */
+  Color MediumAquaMarine();
+
+  /** Returns the color Dim Gray.
+  @returns the color Dim Gray.
+  */
+  Color DimGray();
+
+  /** Returns the color Slate Blue.
+  @returns the color Slate Blue.
+  */
+  Color SlateBlue();
+
+  /** Returns the color Olive Drab.
+  @returns the color Olive Drab.
+  */
+  Color OliveDrab();
+
+  /** Returns the color Slate Gray.
+  @returns the color Slate Gray.
+  */
+  Color SlateGray();
+
+  /** Returns the color Light Slate Gray.
+  @returns the color Light Slate Gray.
+  */
+  Color LightSlateGray();
+
+  /** Returns the color Medium Slate Blue.
+  @returns the color Medium Slate Blue.
+  */
+  Color MediumSlateBlue();
+
+  /** Returns the color Lawn Green.
+  @returns the color Lawn Green.
+  */
+  Color LawnGreen();
+
+  /** Returns the color Chartreuse.
+  @returns the color Chartreuse.
+  */
+  Color Chartreuse();
+
+  /** Returns the color Aquamarine.
+  @returns the color Aquamarine.
+  */
+  Color Aquamarine();
+
+  /** Returns the color Maroon.
+  @returns the color Maroon.
+  */
+  Color Maroon();
+
+  /** Returns the color Purple.
+  @returns the color Purple.
+  */
+  Color Purple();
+
+  /** Returns the color Olive.
+  @returns the color Olive.
+  */
+  Color Olive();
+
+  /** Returns the color Gray.
+  @returns the color Gray.
+  */
+  Color Gray();
+
+  /** Returns the color Sky Blue.
+  @returns the color Sky Blue.
+  */
+  Color SkyBlue();
+
+  /** Returns the color Light Sky Blue.
+  @returns the color Light Sky Blue.
+  */
+  Color LightSkyBlue();
+
+  /** Returns the color Blue Violet.
+  @returns the color Blue Violet.
+  */
+  Color BlueViolet();
+
+  /** Returns the color Dark Red.
+  @returns the color Dar kRed.
+  */
+  Color DarkRed();
+
+  /** Returns the color Dark Magenta.
+  @returns the color Dark Magenta.
+  */
+  Color DarkMagenta();
+
+  /** Returns the color Saddle Brown.
+  @returns the color Saddle Brown.
+  */
+  Color SaddleBrown();
+
+  /** Returns the color Dark Sea Green.
+  @returns the color Dark Sea Green.
+  */
+  Color DarkSeaGreen();
+
+  /** Returns the color Light Green.
+  @returns the color Light Green.
+  */
+  Color LightGreen();
+
+  /** Returns the color Medium Purple.
+  @returns the color Medium Purple.
+  */
+  Color MediumPurple();
+
+  /** Returns the color Dark Violet.
+  @returns the color Dark Violet.
+  */
+  Color DarkViolet();
+
+  /** Returns the color Pale Green.
+  @returns the color Pale Green.
+  */
+  Color PaleGreen();
+
+  /** Returns the color Dark Orchid.
+  @returns the color Dark Orchid.
+  */
+  Color DarkOrchid();
+
+  /** Returns the color Yellow Green.
+  @returns the color Yellow Green.
+  */
+  Color YellowGreen();
+
+  /** Returns the color Sienna.
+  @returns the color Sienna.
+  */
+  Color Sienna();
+
+  /** Returns the color Brown.
+  @returns the color Brown.
+  */
+  Color Brown();
+
+  /** Returns the color Dark Gray.
+  @returns the color Dark Gray.
+  */
+  Color DarkGray();
+
+  /** Returns the color Light Blue.
+  @returns the color Light Blue.
+  */
+  Color LightBlue();
+
+  /** Returns the color Green Yellow.
+  @returns the color Green Yellow.
+  */
+  Color GreenYellow();
+
+  /** Returns the color Pale Turquoise.
+  @returns the color Pale Turquoise.
+  */
+  Color PaleTurquoise();
+
+  /** Returns the color Light Steel Blue.
+  @returns the color Light Steel Blue.
+  */
+  Color LightSteelBlue();
+
+  /** Returns the color Powder Blue.
+  @returns the color Powder Blue.
+  */
+  Color PowderBlue();
+
+  /** Returns the color Fire Brick.
+  @returns the color Fire Brick.
+  */
+  Color FireBrick();
+
+  /** Returns the color Dark Golden Rod.
+  @returns the color Dark Golden Rod.
+  */
+  Color DarkGoldenRod();
+
+  /** Returns the color Medium Orchid.
+  @returns the color Medium Orchid.
+  */
+  Color MediumOrchid();
+
+  /** Returns the color Rosy Brown.
+  @returns the color Rosy Brown.
+  */
+  Color RosyBrown();
+
+  /** Returns the color Dark Khaki.
+  @returns the color Dark Khaki.
+  */
+  Color DarkKhaki();
+
+  /** Returns the color Silver.
+  @returns the color Silver.
+  */
+  Color Silver();
+
+  /** Returns the color Medium Violet Red.
+  @returns the color Medium Violet Red.
+  */
+  Color MediumVioletRed();
+
+  /** Returns the color Indian Red.
+  @returns the color Indian Red.
+  */
+  Color IndianRed();
+
+  /** Returns the color Peru.
+  @returns the color Peru.
+  */
+  Color Peru();
+
+  /** Returns the color Chocolate.
+  @returns the color Chocolate.
+  */
+  Color Chocolate();
+
+  /** Returns the color Tan.
+  @returns the color Tan.
+  */
+  Color Tan();
+
+  /** Returns the color Light Gray.
+  @returns the color Light Gray.
+  */
+  Color LightGray();
+
+  /** Returns the color Thistle.
+  @returns the color Thistle.
+  */
+  Color Thistle();
+
+  /** Returns the color Orchid.
+  @returns the color Orchid.
+  */
+  Color Orchid();
+
+  /** Returns the color Golden Rod.
+  @returns the color Golden Rod.
+  */
+  Color GoldenRod();
+
+  /** Returns the color Pale Violet Red.
+  @returns the color Pale Violet Red.
+  */
+  Color PaleVioletRed();
+
+  /** Returns the color Crimson.
+  @returns the color Crimson.
+  */
+  Color Crimson();
+
+  /** Returns the color Gainsboro.
+  @returns the color Gainsboro.
+  */
+  Color Gainsboro();
+
+  /** Returns the color Plum.
+  @returns the color Plum.
+  */
+  Color Plum();
+
+  /** Returns the color BurlyWood.
+  @returns the color BurlyWood.
+  */
+  Color BurlyWood();
+
+  /** Returns the color LightCyan.
+  @returns the color LightCyan.
+  */
+  Color LightCyan();
+
+  /** Returns the color Lavender.
+  @returns the color Lavender.
+  */
+  Color Lavender();
+
+  /** Returns the color Dark Salmon.
+  @returns the color Dark Salmon.
+  */
+  Color DarkSalmon();
+
+  /** Returns the color Violet.
+  @returns the color Violet.
+  */
+  Color Violet();
+
+  /** Returns the color Pale Golden Rod.
+  @returns the color Pale Golden Rod.
+  */
+  Color PaleGoldenRod();
+
+  /** Returns the color Light Coral.
+  @returns the color Light Coral.
+  */
+  Color LightCoral();
+
+  /** Returns the color Khaki.
+  @returns the color Khaki.
+  */
+  Color Khaki();
+
+  /** Returns the color Alice Blue.
+  @returns the color Alice Blue.
+  */
+  Color AliceBlue();
+
+  /** Returns the color Honey Dew.
+  @returns the color Honey Dew.
+  */
+  Color HoneyDew();
+
+  /** Returns the color Azure.
+  @returns the color Azure.
+  */
+  Color Azure();
+
+  /** Returns the color Sandy Brown.
+  @returns the color Sandy Brown.
+  */
+  Color SandyBrown();
+
+  /** Returns the color Wheat.
+  @returns the color Wheat.
+  */
+  Color Wheat();
+
+  /** Returns the color Beige.
+  @returns the color Beige.
+  */
+  Color Beige();
+
+  /** Returns the color White Smoke.
+  @returns the color White Smoke.
+  */
+  Color WhiteSmoke();
+
+  /** Returns the color Mint Cream.
+  @returns the color Mint Cream.
+  */
+  Color MintCream();
+
+  /** Returns the color Ghost White.
+  @returns the color Ghost White.
+  */
+  Color GhostWhite();
+
+  /** Returns the color Salmon.
+  @returns the color Salmon.
+  */
+  Color Salmon();
+
+  /** Returns the color Antique White.
+  @returns the color Antique White.
+  */
+  Color AntiqueWhite();
+
+  /** Returns the color Linen.
+  @returns the color Linen.
+  */
+  Color Linen();
+
+  /** Returns the color Light Golden Rod Yellow.
+  @returns the color Light Golden Rod Yellow.
+  */
+  Color LightGoldenRodYellow();
+
+  /** Returns the color Old Lace.
+  @returns the color Old Lace.
+  */
+  Color OldLace();
+
+  /** Returns the color Red.
+  @returns the color Red.
+  */
+  Color Red();
+
+  /** Returns the color Fuchsia.
+  @returns the color Fuchsia.
+  */
+  Color Fuchsia();
+
+  /** Returns the color Magenta.
+  @returns the color Magenta.
+  */
+  Color Magenta();
+
+  /** Returns the color Deep Pink.
+  @returns the color Deep Pink.
+  */
+  Color DeepPink();
+
+  /** Returns the color Orange Red.
+  @returns the color Orange Red.
+  */
+  Color OrangeRed();
+
+  /** Returns the color Tomato.
+  @returns the color Tomato.
+  */
+  Color Tomato();
+
+  /** Returns the color Hot Pink.
+  @returns the color Hot Pink.
+  */
+  Color HotPink();
+
+  /** Returns the color Coral.
+  @returns the color Coral.
+  */
+  Color Coral();
+
+  /** Returns the color Dark Orange.
+  @returns the color Dark Orange.
+  */
+  Color DarkOrange();
+
+  /** Returns the color Light Salmon.
+  @returns the color Light Salmon.
+  */
+  Color LightSalmon();
+
+  /** Returns the color Orange.
+  @returns the color Orange.
+  */
+  Color Orange();
+
+  /** Returns the color Light Pink.
+  @returns the color Light Pink.
+  */
+  Color LightPink();
+
+  /** Returns the color Pink.
+  @returns the color Pink.
+  */
+  Color Pink();
+
+  /** Returns the color Gold.
+  @returns the color Gold.
+  */
+  Color Gold();
+
+  /** Returns the color Peach Puff.
+  @returns the color Peach Puff.
+  */
+  Color PeachPuff();
+
+  /** Returns the color Navajo White.
+  @returns the color Navajo White.
+  */
+  Color NavajoWhite();
+
+  /** Returns the color Moccasin.
+  @returns the color Moccasin.
+  */
+  Color Moccasin();
+
+  /** Returns the color Bisque.
+  @returns the color Bisque.
+  */
+  Color Bisque();
+
+  /** Returns the color Misty Rose.
+  @returns the color Misty Rose.
+  */
+  Color MistyRose();
+
+  /** Returns the color Blanched Almond.
+  @returns the color Blanched Almond.
+  */
+  Color BlanchedAlmond();
+
+  /** Returns the color Papaya Whip.
+  @returns the color Papaya Whip.
+  */
+  Color PapayaWhip();
+
+  /** Returns the color Lavender Blush.
+  @returns the color Lavender Blush.
+  */
+  Color LavenderBlush();
+
+  /** Returns the color Sea Shell.
+  @returns the color Sea Shell.
+  */
+  Color SeaShell();
+
+  /** Returns the color Cornsilk.
+  @returns the color Cornsilk.
+  */
+  Color Cornsilk();
+
+  /** Returns the color Lemon Chiffon.
+  @returns the color Lemon Chiffon.
+  */
+  Color LemonChiffon();
+
+  /** Returns the color Floral White.
+  @returns the color Floral White.
+  */
+  Color FloralWhite();
+
+  /** Returns the color Snow.
+  @returns the color Snow.
+  */
+  Color Snow();
+
+  /** Returns the color Yellow.
+  @returns the color Yellow.
+  */
+  Color Yellow();
+
+  /** Returns the color Light Yellow.
+  @returns the color Light Yellow.
+  */
+  Color LightYellow();
+
+  /** Returns the color Ivory.
+  @returns the color Ivory.
+  */
+  Color Ivory();
+
+  /** Returns the color White.
+  @returns the color White.
+  */
+  Color White();
+
+  /** @} */
+}  // namespace colors
+
+#endif  // EUPHORIA_COLORS_H_
