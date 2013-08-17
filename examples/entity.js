c6f1e531f0a0247216123883a8470a7491097545
Gustav <sir.gustav.the.coder@gmail.com>
2013-08-17 18:10:16 +0200

    correctly rotated fighter

--- examples/entity.js
+++ examples/entity.js
@@ -1,9 +1,9 @@
 [
 	 { "name" : "craft",  "data": [
 		 {"system": "Rendering",
-			"data" : {"file": "fighter1.3ds"}}
+			"data" : {"file": "fighter1.obj"}}
 		,{"system": "Physics",
-			"data" : {"mass":1, "width":18, "height":4, "depth":25}}
+			"data" : {"mass":1, "width":25, "height":4, "depth":18}}
 		,{"system": "test", "data": {"force": 50}}
 	] }
 	
