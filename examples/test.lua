76ec8e3faa881d26fb4997740c172b23fa5c607d
Gustav <sir.gustav.the.coder@gmail.com>
2013-08-17 18:11:08 +0200

    basic game code

--- examples/test.lua
+++ examples/test.lua
@@ -10,24 +10,36 @@ function test_step(self, entity, dt)
 	local phys = Physics.Get(entity)
 	--Physics.ApplyForce(phys, -100*mousey*self.force, 0, -100*mousex*self.force)
 	if yawpkey > 0.5 then
-		Physics.ApplyTorque(phys, 0, self.force, 0)
+		local q = Physics.GetOrientation(phys)
+		local a = q:gety()
+		Physics.ApplyTorque(phys, a*self.force)
 	end
 	if yawnkey > 0.5 then
-		Physics.ApplyTorque(phys, 0, -self.force, 0)
+		local q = Physics.GetOrientation(phys)
+		local a = q:gety()
+		Physics.ApplyTorque(phys, a*-self.force)
 	end
 	
 	if pitchpkey > 0.5 then
-		Physics.ApplyTorque(phys, self.force, 0, 0)
+		local q = Physics.GetOrientation(phys)
+		local a = q:getx()
+		Physics.ApplyTorque(phys, a*self.force)
 	end
 	if pitchnkey > 0.5 then
-		Physics.ApplyTorque(phys, -self.force, 0, 0)
+		local q = Physics.GetOrientation(phys)
+		local a = q:getx()
+		Physics.ApplyTorque(phys, a*-self.force)
 	end
 	
 	if rollpkey > 0.5 then
-		Physics.ApplyTorque(phys, 0, 0, self.force)
+		local q = Physics.GetOrientation(phys)
+		local a = q:getz()
+		Physics.ApplyTorque(phys, a*self.force)
 	end
 	if rollnkey > 0.5 then
-		Physics.ApplyTorque(phys, 0, 0, -self.force)
+		local q = Physics.GetOrientation(phys)
+		local a = q:getz()
+		Physics.ApplyTorque(phys, a*-self.force)
 	end
 	
 	if forcekey > 0.5 then
@@ -35,6 +47,11 @@ function test_step(self, entity, dt)
 		local a = q:gety()
 		Physics.ApplyForce(phys, a*self.force)
 	end
+	
+	if resetkey > 0.5 then
+		Physics.SetOrientation(phys, cquat.identity())
+	end
+	
 	if quitkey > 0.5 then
 		Game.Quit()
 	end
