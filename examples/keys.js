3fc2bffc1dd46cb7665715e7e293396136f9bbfe
Gustav <sir.gustav.the.coder@gmail.com>
2013-08-18 12:01:00 +0200

    better key binding

--- examples/keys.js
+++ examples/keys.js
@@ -2,10 +2,10 @@
 	{"name":"force", "scriptvar":"forcekey", "key":"Space"}
 	, {"name":"yaw+", "scriptvar":"yawpkey", "key":"D"}
 	, {"name":"yaw-", "scriptvar":"yawnkey", "key":"A"}
-	, {"name":"pitch+", "scriptvar":"pitchpkey", "key":"S"}
-	, {"name":"pitch-", "scriptvar":"pitchnkey", "key":"W"}
-	, {"name":"roll+", "scriptvar":"rollpkey", "key":"E"}
-	, {"name":"roll-", "scriptvar":"rollnkey", "key":"Q"}
+	, {"name":"pitch+", "scriptvar":"pitchpkey", "key":"W"}
+	, {"name":"pitch-", "scriptvar":"pitchnkey", "key":"S"}
+	, {"name":"roll+", "scriptvar":"rollpkey", "key":"Q"}
+	, {"name":"roll-", "scriptvar":"rollnkey", "key":"E"}
 	, {"name":"reset", "scriptvar":"resetkey", "key":"R"}
 	, {"name":"quit", "scriptvar":"quitkey", "key":"Esc"}
 ]
