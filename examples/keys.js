8c88966c29db63a798296d0a63c1bb8331ad6a01
Gustav <sir.gustav.the.coder@gmail.com>
2013-08-19 17:15:04 +0200

    added joystick controls

--- examples/keys.js
+++ examples/keys.js
@@ -20,19 +20,19 @@
 	,{
 		"name": "default"
 		,"keys": [
-			{"action":"force", "key":"JoystickR+", "device":0}
-			, {"action":"yaw+", "key":"JoystickX+", "device":0}
-			, {"action":"yaw-", "key":"JoystickX-", "device":0}
-			, {"action":"pitch+", "key":"JoystickY+", "device":0}
-			, {"action":"pitch-", "key":"JoystickY-", "device":0}
-			, {"action":"roll+","key":"JoystickZ+", "device":0}
-			, {"action":"roll-","key":"JoystickZ-", "device":0}
+			{"action":"force", "key":"JoystickZ-", "device":0}
+			, {"action":"yaw+", "key":"JoystickR+", "device":0}
+			, {"action":"yaw-", "key":"JoystickR-", "device":0}
+			, {"action":"pitch+", "key":"JoystickY-", "device":0}
+			, {"action":"pitch-", "key":"JoystickY+", "device":0}
+			, {"action":"roll+","key":"JoystickX-", "device":0}
+			, {"action":"roll-","key":"JoystickX+", "device":0}
 			, {"action":"reset","key":"R", "device":0}
 			, {"action":"quit", "key":"Esc", "device":0}
-			, {"action":"camright", "key":"JoystickPovX+", "device":0}
-			, {"action":"camleft", "key":"JoystickPovX-", "device":0}
-			, {"action":"camup", "key":"JoystickPovY-", "device":0}
-			, {"action":"camdown", "key":"JoystickPovY+", "device":0}
+			, {"action":"camright", "key":"JoystickPovY+", "device":0}
+			, {"action":"camleft", "key":"JoystickPovY-", "device":0}
+			, {"action":"camup", "key":"JoystickPovX+", "device":0}
+			, {"action":"camdown", "key":"JoystickPovX-", "device":0}
 		]
 	}
 ]
--- examples/test.lua
+++ examples/test.lua
@@ -12,34 +12,34 @@ function test_step(self, entity, dt)
 	if yawpkey > 0.5 then
 		local q = Physics.GetOrientation(phys)
 		local a = q:gety()
-		Physics.ApplyTorque(phys, a*self.force)
+		Physics.ApplyTorque(phys, a*self.force*yawpkey)
 	end
-	if yawnkey > 0.5 then
+	if yawnkey > 0.0 then
 		local q = Physics.GetOrientation(phys)
 		local a = q:gety()
-		Physics.ApplyTorque(phys, a*-self.force)
+		Physics.ApplyTorque(phys, a*-self.force*yawnkey)
 	end
 	
 	if pitchpkey > 0.5 then
 		local q = Physics.GetOrientation(phys)
 		local a = q:getx()
-		Physics.ApplyTorque(phys, a*self.force)
+		Physics.ApplyTorque(phys, a*self.force*pitchpkey)
 	end
-	if pitchnkey > 0.5 then
+	if pitchnkey > 0.0 then
 		local q = Physics.GetOrientation(phys)
 		local a = q:getx()
-		Physics.ApplyTorque(phys, a*-self.force)
+		Physics.ApplyTorque(phys, a*-self.force*pitchnkey)
 	end
 	
-	if rollpkey > 0.5 then
+	if rollpkey > 0.0 then
 		local q = Physics.GetOrientation(phys)
 		local a = q:getz()
-		Physics.ApplyTorque(phys, a*self.force)
+		Physics.ApplyTorque(phys, a*self.force*rollpkey)
 	end
-	if rollnkey > 0.5 then
+	if rollnkey > 0.0 then
 		local q = Physics.GetOrientation(phys)
 		local a = q:getz()
-		Physics.ApplyTorque(phys, a*-self.force)
+		Physics.ApplyTorque(phys, a*-self.force*rollnkey)
 	end
 	
 	if forcekey > 0.0 then
