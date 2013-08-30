5a01ba7f4bb31dae35f258977baa623dee15cbe4
Gustav <sir.gustav.the.coder@gmail.com>
2013-08-30 08:13:01 +0200

    added basic key support

--- euphoria/main/main-sdl2.cc
+++ euphoria/main/main-sdl2.cc
@@ -373,6 +373,216 @@ class Context : boost::noncopyable {
     SDL_GLContext context_;
 };
 
+Key::Type ToKey(SDL_Keysym key) {
+  switch (key.sym) {
+    case SDLK_a:
+      return Key::A;
+    case SDLK_b:
+      return Key::B;
+    case SDLK_c:
+      return Key::C;
+    case SDLK_d:
+      return Key::D;
+    case SDLK_e:
+      return Key::E;
+    case SDLK_f:
+      return Key::F;
+    case SDLK_g:
+      return Key::G;
+    case SDLK_h:
+      return Key::H;
+    case SDLK_i:
+      return Key::I;
+    case SDLK_j:
+      return Key::J;
+    case SDLK_k:
+      return Key::K;
+    case SDLK_l:
+      return Key::L;
+    case SDLK_m:
+      return Key::M;
+    case SDLK_n:
+      return Key::N;
+    case SDLK_o:
+      return Key::O;
+    case SDLK_p:
+      return Key::P;
+    case SDLK_q:
+      return Key::Q;
+    case SDLK_r:
+      return Key::R;
+    case SDLK_s:
+      return Key::S;
+    case SDLK_t:
+      return Key::T;
+    case SDLK_u:
+      return Key::U;
+    case SDLK_v:
+      return Key::V;
+    case SDLK_w:
+      return Key::W;
+    case SDLK_x:
+      return Key::X;
+    case SDLK_y:
+      return Key::Y;
+    case SDLK_z:
+      return Key::Z;
+    case SDLK_0:
+      return Key::Num0;
+    case SDLK_1:
+      return Key::Num1;
+    case SDLK_2:
+      return Key::Num2;
+    case SDLK_3:
+      return Key::Num3;
+    case SDLK_4:
+      return Key::Num4;
+    case SDLK_5:
+      return Key::Num5;
+    case SDLK_6:
+      return Key::Num6;
+    case SDLK_7:
+      return Key::Num7;
+    case SDLK_8:
+      return Key::Num8;
+    case SDLK_9:
+      return Key::Num9;
+    case SDLK_ESCAPE:
+      return Key::Escape;
+    case SDLK_LCTRL:
+      return Key::LControl;
+    case SDLK_LSHIFT:
+      return Key::LShift;
+    case SDLK_LALT:
+      return Key::LAlt;
+    case SDLK_LGUI:
+      return Key::LSystem;
+    case SDLK_RCTRL:
+      return Key::RControl;
+    case SDLK_RSHIFT:
+      return Key::RShift;
+    case SDLK_RALT:
+      return Key::RAlt;
+    case SDLK_RGUI:
+      return Key::RSystem;
+    case SDLK_MENU:
+      return Key::Menu;
+    case SDLK_LEFTBRACKET:
+      return Key::LBracket;
+    case SDLK_RIGHTBRACKET:
+      return Key::RBracket;
+    case SDLK_SEMICOLON:
+      return Key::SemiColon;
+    case SDLK_COMMA:
+      return Key::Comma;
+    case SDLK_PERIOD:
+      return Key::Period;
+    case SDLK_QUOTE:
+      return Key::Quote;
+    case SDLK_SLASH:
+      return Key::Slash;
+    case SDLK_BACKSLASH:
+      return Key::BackSlash;
+    case SDLK_BACKQUOTE:
+      return Key::Tilde;
+    case SDLK_EQUALS:
+      return Key::Equal;
+    case SDLK_MINUS:
+      return Key::Dash;
+    case SDLK_SPACE:
+      return Key::Space;
+    case SDLK_RETURN:
+      return Key::Return;
+    case SDLK_BACKSPACE:
+      return Key::BackSpace;
+    case SDLK_TAB:
+      return Key::Tab;
+    case SDLK_PAGEUP:
+      return Key::PageUp;
+    case SDLK_PAGEDOWN:
+      return Key::PageDown;
+    case SDLK_END:
+      return Key::End;
+    case SDLK_HOME:
+      return Key::Home;
+    case SDLK_INSERT:
+      return Key::Insert;
+    case SDLK_DELETE:
+      return Key::Delete;
+    case SDLK_KP_PLUS:
+      return Key::Add;
+    case SDLK_KP_MINUS:
+      return Key::Subtract;
+    case SDLK_KP_MULTIPLY:
+      return Key::Multiply;
+    case SDLK_KP_DIVIDE:
+      return Key::Divide;
+    case SDLK_LEFT:
+      return Key::Left;
+    case SDLK_RIGHT:
+      return Key::Right;
+    case SDLK_UP:
+      return Key::Up;
+    case SDLK_DOWN:
+      return Key::Down;
+    case SDLK_KP_0:
+      return Key::Numpad0;
+    case SDLK_KP_1:
+      return Key::Numpad1;
+    case SDLK_KP_2:
+      return Key::Numpad2;
+    case SDLK_KP_3:
+      return Key::Numpad3;
+    case SDLK_KP_4:
+      return Key::Numpad4;
+    case SDLK_KP_5:
+      return Key::Numpad5;
+    case SDLK_KP_6:
+      return Key::Numpad6;
+    case SDLK_KP_7:
+      return Key::Numpad7;
+    case SDLK_KP_8:
+      return Key::Numpad8;
+    case SDLK_KP_9:
+      return Key::Numpad9;
+    case SDLK_F1 :
+      return Key::F1;
+    case SDLK_F2 :
+      return Key::F2;
+    case SDLK_F3 :
+      return Key::F3;
+    case SDLK_F4 :
+      return Key::F4;
+    case SDLK_F5 :
+      return Key::F5;
+    case SDLK_F6 :
+      return Key::F6;
+    case SDLK_F7 :
+      return Key::F7;
+    case SDLK_F8 :
+      return Key::F8;
+    case SDLK_F9 :
+      return Key::F9;
+    case SDLK_F10:
+      return Key::F10;
+    case SDLK_F11:
+      return Key::F11;
+    case SDLK_F12:
+      return Key::F12;
+    case SDLK_F13:
+      return Key::F13;
+    case SDLK_F14:
+      return Key::F14;
+    case SDLK_F15:
+      return Key::F15;
+    case SDLK_PAUSE:
+      return Key::Pause;
+    default:
+      assert(0 && key.sym && "Invalid keyboard button");
+      return Key::Invalid;
+  }
+}
+
 void logic() {
   SDL_LogSetPriority(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_INFO);
   Settings settings;
@@ -455,7 +665,9 @@ void logic() {
       if (event.type == SDL_QUIT) {
         game.Quit();
       }
-      if (event.type == SDL_KEYDOWN) {
+      if (event.type == SDL_KEYDOWN || event.type == SDL_KEYUP) {
+        const bool down = event.type == SDL_KEYDOWN;
+        game.OnKey(ToKey(event.key.keysym), 0, down);
         if (event.key.keysym.sym == SDLK_ESCAPE) {
           game.Quit();
         }
