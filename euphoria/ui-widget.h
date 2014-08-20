0dc27134e41aeffaefeae7768e073d8f940abade
Gustav <sir.gustav.the.coder@gmail.com>
2014-08-20 15:17:36 +0200

    added stuff to the widget

--- euphoria/ui-widget.h
+++ euphoria/ui-widget.h
@@ -7,6 +7,8 @@ Camera related code.
 #ifndef EUPHORIA_UI_WIDGET_H_
 #define EUPHORIA_UI_WIDGET_H_
 
+#include "euphoria/math.h"
+
 namespace euphoria {
 namespace ui {
 class Widget {
@@ -15,9 +17,14 @@ class Widget {
   virtual ~Widget();
   virtual void Draw() = 0;
 
+  Vec2 position() const;
+  void set_position(const Vec2& position);
+
+  Vec2 size() const;
+  void set_size(const Vec2& size);
  private:
-  // position
-  // size
+  Vec2 position_;
+  Vec2 size_;
 };
 }  // namespace ui
 }  // namespace euphoria
