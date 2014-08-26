37d3792d65e121827cc5f58338ffafe87372246c
Gustav <sir.gustav.the.coder@gmail.com>
2014-08-26 21:43:40 +0200

    removed boost stdint in favor of c++11 types

--- euphoria/ints.h
+++ euphoria/ints.h
@@ -7,7 +7,7 @@ Definitions for various integers.
 #ifndef EUPHORIA_INTS_H_
 #define EUPHORIA_INTS_H_
 
-#include <boost/cstdint.hpp>
+#include <cstdint>
 
 namespace euphoria {
 
@@ -19,111 +19,111 @@ namespace euphoria {
 
 /** Signed integer, exactly 64 bytes.
  */
-typedef boost::int64_t int64;
+typedef std::int64_t int64;
 
 /** Unsigned integer, exactly 32 bytes.
  */
-typedef boost::uint64_t uint64;
+typedef std::uint64_t uint64;
 
 /** Signed integer, exactly 32 bytes.
  */
-typedef boost::int32_t int32;
+typedef std::int32_t int32;
 
 /** Unsigned integer, exactly 32 bytes.
  */
-typedef boost::uint32_t uint32;
+typedef std::uint32_t uint32;
 
 /** Signed integer, exactly 16 bytes.
  */
-typedef boost::int16_t int16;
+typedef std::int16_t int16;
 
 /** Unsigned integer, exactly 16 bytes.
  */
-typedef boost::uint16_t uint16;
+typedef std::uint16_t uint16;
 
 /** Signed integer, exactly 8 bytes.
  */
-typedef boost::int8_t int8;
+typedef std::int8_t int8;
 
 /** Unsigned integer, exactly 8 bytes.
  */
-typedef boost::uint8_t uint8;
+typedef std::uint8_t uint8;
 
 ///////////////////////////////////////////////////////////////////////////////
 
 /** Signed integer, smallest data type with at least 64 bytes
  */
-typedef boost::int_least64_t lint64;
+typedef std::int_least64_t lint64;
 
 /** Unsigned integer, smallest data type with at least 64 bytes
  */
-typedef boost::uint_least64_t luint64;
+typedef std::uint_least64_t luint64;
 
 /** Signed integer, smallest data type with at least 32 bytes
  */
-typedef boost::int_least32_t lint32;
+typedef std::int_least32_t lint32;
 
 /** Unsigned integer, smallest data type with at least 32 bytes
  */
-typedef boost::uint_least32_t luint32;
+typedef std::uint_least32_t luint32;
 
 /** Signed integer, smallest data type with at least 16 bytes
  */
-typedef boost::int_least16_t lint16;
+typedef std::int_least16_t lint16;
 
 /** Unsigned integer, smallest data type with at least 16 bytes
  */
-typedef boost::uint_least16_t luint16;
+typedef std::uint_least16_t luint16;
 
 /** Signed integer, smallest data type with at least 8 bytes
  */
-typedef boost::int_least8_t lint8;
+typedef std::int_least8_t lint8;
 
 /** Unsigned integer, smallest data type with at least 8 bytes
  */
-typedef boost::uint_least8_t luint8;
+typedef std::uint_least8_t luint8;
 
 ///////////////////////////////////////////////////////////////////////////////
 
 /** Signed integer, smallest data type with at least 64 bytes and best
  * arithmetic performance.
  */
-typedef boost::int_fast64_t fint64;
+typedef std::int_fast64_t fint64;
 
 /** Unsigned integer, smallest data type with at least 64 bytes and best
  * arithmetic performance.
  */
-typedef boost::uint_fast64_t fuint64;
+typedef std::uint_fast64_t fuint64;
 
 /** Signed integer, smallest data type with at least 32 bytes and best
  * arithmetic performance.
  */
-typedef boost::int_fast32_t fint32;
+typedef std::int_fast32_t fint32;
 
 /** Unsigned integer, smallest data type with at least 32 bytes and best
  * arithmetic performance.
  */
-typedef boost::uint_fast32_t fuint32;
+typedef std::uint_fast32_t fuint32;
 
 /** Signed integer, smallest data type with at least 16 bytes and best
  * arithmetic performance.
  */
-typedef boost::int_fast16_t fint16;
+typedef std::int_fast16_t fint16;
 
 /** Unsigned integer, smallest data type with at least 16 bytes and best
  * arithmetic performance.
  */
-typedef boost::uint_fast16_t fuint16;
+typedef std::uint_fast16_t fuint16;
 
 /** Signed integer, smallest data type with at least 8 bytes and best arithmetic
  * performance.
  */
-typedef boost::int_fast8_t fint8;
+typedef std::int_fast8_t fint8;
 
 /** Unsigned integer, smallest data type with at least 8 bytes and best
  * arithmetic performance.
  */
-typedef boost::uint_fast8_t fuint8;
+typedef std::uint_fast8_t fuint8;
 
 /** @} */
 
